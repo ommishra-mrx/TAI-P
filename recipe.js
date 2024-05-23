f57d0af544bfe8865655eff6d85d9238c3417420
Om Mishra <168508721+ommishra-mrx@users.noreply.github.com>
2024-05-23 22:23:18 +0530

    Add files via upload

---
+++ recipe.js
@@ -0,0 +1,19 @@
+function toggleIngredients1() 
+{
+    var ingredients = document.getElementById("ingredients1");
+    if (ingredients.style.display === "none") {
+         ingredients.style.display = "block";
+        } 
+    else {
+                ingredients.style.display = "none";
+        }
+}
+function toggleIngredients2() {
+    var ingredients = document.getElementById("ingredients2");
+    if (ingredients.style.display === "none") {
+        ingredients.style.display = "block";
+       } 
+    else {
+                ingredients.style.display = "none";
+        }
+}
